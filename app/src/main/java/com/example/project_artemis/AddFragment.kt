f8af781cd4036e10bc40429015a3ead9f97e3624
kyletagulao <20-03001@g.batstate-u.edu.ph>
2023-04-06 14:28:59 +0800

    Status Fragment

--- app/src/main/java/com/example/project_artemis/AddFragment.kt
+++ app/src/main/java/com/example/project_artemis/AddFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private lateinit var spinnerLocation: Spinner
+private lateinit var itemsLocation: List<String>
 
 /**
  * A simple [Fragment] subclass.
@@ -35,6 +40,25 @@ class AddFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        spinnerLocation = view?.findViewById(R.id.locationPickerInput)!!
+
+        itemsLocation = listOf("Batangas State University - Alangilan", "Batangas State University - Pablo Borbon", "Batangas State University - Malvar")
+
+        val adapter: ArrayAdapter<*> = ArrayAdapter<Any?>(
+            requireContext().applicationContext,
+            R.layout.spinner_selected_item2,
+            itemsLocation
+        )
+
+        adapter.setDropDownViewResource(R.layout.style_spinner)
+
+        spinnerLocation.adapter = adapter
+
     }
 
     companion object {
--- app/src/main/java/com/example/project_artemis/DataFragment.kt
+++ app/src/main/java/com/example/project_artemis/DataFragment.kt
@@ -1,15 +1,25 @@
 package com.example.project_artemis
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private lateinit var datePickerDialog: DatePickerDialog
+@SuppressLint("StaticFieldLeak")
+private lateinit var datePickerButton: Button
 
 /**
  * A simple [Fragment] subclass.
@@ -35,6 +45,93 @@ class DataFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_data, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initDatePicker()
+        datePickerButton = view.findViewById(R.id.datePickerData)
+        datePickerButton.setText(getTodaysDate())
+
+        datePickerButton.setOnClickListener {
+            datePickerDialog.show()
+        }
+    }
+
+    private fun initDatePicker() {
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+                var month = month
+                month = month + 1
+                val date: String = makeDateString(day, month, year)
+                datePickerButton.setText(date)
+            }
+
+        val calendar: Calendar = Calendar.getInstance()
+        val year: Int = calendar.get(Calendar.YEAR)
+        val month: Int = calendar.get(Calendar.MONTH)
+        val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val style: Int = AlertDialog.THEME_HOLO_LIGHT
+
+        datePickerDialog = DatePickerDialog(view?.context!!, style, dateSetListener, year, month, day)
+
+    }
+
+    private fun makeDateString(day: Int, month: Int, year: Int): String {
+        return getMonthFormat(month) + " " + day + " " + year
+    }
+
+    private fun getMonthFormat(month: Int): String {
+        if (month == 1) {
+            return "JAN"
+        }
+        if (month == 2) {
+            return "FEB"
+        }
+        if (month == 3) {
+            return "MAR"
+        }
+        if (month == 4) {
+            return "APR"
+        }
+        if (month == 5) {
+            return "MAY"
+        }
+        if (month == 6) {
+            return "JUN"
+        }
+        if (month == 7) {
+            return "JUL"
+        }
+        if (month == 8) {
+            return "AUG"
+        }
+        if (month == 9) {
+            return "SEP"
+        }
+        if (month == 10) {
+            return "OCT"
+        }
+        if (month == 11) {
+            return "NOV"
+        }
+        if (month == 12) {
+            return "DEC"
+        }
+
+        return "JAN"
+    }
+
+    private fun getTodaysDate(): String {
+        val calendar: Calendar = Calendar.getInstance()
+        val year: Int = calendar.get(Calendar.YEAR)
+        var month: Int = calendar.get(Calendar.MONTH)
+        month = month + 1
+        val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return makeDateString(day, month, year)
     }
 
     companion object {
