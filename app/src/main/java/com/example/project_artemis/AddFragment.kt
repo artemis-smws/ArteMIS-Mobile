e9e978247d7c21e7221e0b5ecd8d734862477513
Amboyandrey <andreyamboy13@gmail.com>
2023-05-05 09:53:44 +0800

    not yet

--- app/src/main/java/com/example/project_artemis/AddFragment.kt
+++ app/src/main/java/com/example/project_artemis/AddFragment.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.appcompat.app.AlertDialog
 import com.example.project_artemis.databinding.FragmentAddBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
 import org.json.JSONObject
-import java.net.URL
-import java.net.HttpURLConnection
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
 
 class AddFragment : Fragment() {
 
@@ -31,6 +32,7 @@ class AddFragment : Fragment() {
         binding.amountText.text = getString(R.string.amountText)
         binding.typeText.text = getString(R.string.typeText)
         binding.quantityText.text = getString(R.string.quantityText)
+        binding.NameText.text = getString(R.string.nameText)
 
         val itemsLocation = listOf(
             "Batangas State University - Alangilan",
@@ -48,60 +50,73 @@ class AddFragment : Fragment() {
 
         binding.locationPickerInput.adapter = adapter
 
-binding.inputButton.setOnClickListener {
-    
-    val waste_type = binding.typeeditText.text.toString().trim()
-    val name = binding.nameeditText.text.toString().trim()
-    val quantity = binding.quantityeditText.text.toString().toInt().trim()
-    val weight = binding.amountedditText.text.toString().toInt().trim()
+        binding.inputButton.setOnClickListener {
 
-    val requestBody = JSONObject()
-    val item = JSONObject()
-    val itemsArray = JSONArray()
+            val wastetype = binding.typeeditText.text.toString().trim()
+            val name = binding.nameeditText.text.toString().trim()
+            val quantity = binding.quantityeditText.text.toString().trim().toInt()
+            val weight = binding.amountedditText.text.toString().trim().toInt()
 
-    item.put("name", name)
-    item.put("quantity", quantity)
+            val requestBody = JSONObject()
+            val item = JSONObject()
+            val itemsArray = JSONArray()
 
-    itemsArray.put(item)
+            item.put("name", name)
+            item.put("quantity", quantity)
 
-    val editTextObject = JSONObject()
-    editTextObject.put("items", itemsArray)
-    editTextObject.put("weight", weight)
+            itemsArray.put(item)
 
-    requestBody.put(waste_type, editTextObject)
+            val editTextObject = JSONObject()
+            editTextObject.put("items", itemsArray)
+            editTextObject.put("weight", weight)
 
-    Thread {
-        try {
-            val url = URL("https://us-central1-artemis-b18ae.cloudfunctions.net/server/waste")
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "POST"
-            connection.setRequestProperty("Content-Type", "application/json; utf-8")
-            connection.setRequestProperty("Accept", "application/json")
-            connection.doOutput = true
+            requestBody.put(wastetype, editTextObject)
 
-            val outputStream = connection.outputStream
-            val input = BufferedWriter(OutputStreamWriter(outputStream, "UTF-8"))
-            input.write(requestBody.toString())
-            input.close()
-            outputStream.close()
-
-            val responseCode = connection.responseCode
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val responseStream = connection.inputStream
-                val response = BufferedReader(InputStreamReader(responseStream)).readText()
-                responseStream.close()
-                //if it work
-            } else {
-                //if it doesnt
+            GlobalScope.launch(Dispatchers.IO) {
+                try {
+                    val client = OkHttpClient()
+                    val mediaType = "application/json; charset=utf-8".toMediaType()
+                    val request = Request.Builder()
+                        .url("https://us-central1-artemis-b18ae.cloudfunctions.net/server/waste")
+                        .post(requestBody.toString().toRequestBody(mediaType))
+                        .build()
+                    val response = client.newCall(request).execute()
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        withContext(Dispatchers.Main) {
+                            // Update UI or show success message
+                            binding.typeeditText.setText("")
+                            binding.nameeditText.setText("")
+                            binding.quantityeditText.setText("")
+                            binding.amountedditText.setText("")
+                        }
+                    } else {
+                        // handle the error here
+                        Toast.makeText(requireContext(), "Error: ${response.code}", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(requireContext(), "An error occurred: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-            connection.disconnect()
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
-    }.start()
-}
 
         return binding.root
     }
-
 }
+/*
+Copyright 2023 arteMIS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
